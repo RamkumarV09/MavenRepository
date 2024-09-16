892970494b80c625cf262a95c276898f2ba1d57c
RamkumarV09 <ramkumarv0986@gmail.com>
2024-09-16 20:11:27 +0530

    First Code Push

---
+++ src/test/java/org/sample/SampleGIT.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+public class SampleGIT {
+	
+	private void mobile() {
+
+		
+		System.out.println("Mobile");
+	}
+	
+	private void tv() {
+
+		System.out.println("Tv");
+	}
+	
+	private void washingMachine() {
+
+		System.out.println("Washing Machine");
+	}
+
+}
